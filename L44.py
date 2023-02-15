2930996f54db7676c8106079ad56fc42c4ae59cc
Gabby F G <123026505+GFGallaway@users.noreply.github.com>
2023-02-15 16:00:12 -0500

    Add files via upload

---
+++ L44.py
@@ -0,0 +1,8 @@
+#authors are Gabby and Emily
+for x in ["paper", "rock", "paper", "scissor", "rock", "rock", "scissor", "paper"]:
+    if x=="paper":
+        print("Paper eats rock.")
+    if x=="rock":
+        print("Rock hits scissor.")
+    if x=="scissor":
+        print("Scissor beats paper.")
